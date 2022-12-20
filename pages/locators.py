63feeabaef6ca15008fbce8567448bedaf78eee4
AvaOleh <avakimyanov@gmail.com>
2022-12-21 02:47:00 +0300

    add login page and locators

---
+++ pages/locators.py
@@ -0,0 +1,13 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators():
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    # REGISTER_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
+    # REGISTER_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password1")
+    # REGISTER_CONFIRM_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password2")
+    # REGISTER_SUBMIT = (By.CSS_SELECTOR, "button[name='registration_submit']")
--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,4 +1,5 @@
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
 from selenium.webdriver.common.by import By
 
 
@@ -6,4 +7,5 @@ def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()  # открываем страницу
-    page.should_be_login_link()  # выполняем метод страницы — переходим на страницу логина
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
