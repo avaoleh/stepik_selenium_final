c96718b9e84407fbdbfb35f6512964492d851c39
AvaOleh <avakimyanov@gmail.com>
2022-12-22 00:09:20 +0300

    refactoring

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -24,14 +24,6 @@ class TestLoginFromMainPage():
         login_page = LoginPage(browser, browser.current_url)
         login_page.should_be_login_page()
 
-
-def test_guest_can_go_to_login_page(browser):
-    link = link_main_page
-    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()  # открываем страницу
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
-
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser, user_language):
     link = link_main_page
     page = MainPage(browser, link)
