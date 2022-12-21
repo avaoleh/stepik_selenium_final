73529daa1a5f87e02b6e5fb9b29167ce073cedc2
AvaOleh <avakimyanov@gmail.com>
2022-12-21 23:19:56 +0300

    group tests for login from main page into the class

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,9 +1,29 @@
+import pytest
+
 from pages.main_page import MainPage
 from pages.login_page import LoginPage
 from pages.basket_page import BasketPage
 from pages.basket_page import EMPTY_BASKET_MESSAGES
 
 link_main_page = "http://selenium1py.pythonanywhere.com/"
+
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self):
+        self.link = "http://selenium1py.pythonanywhere.com/"
+    def test_guest_can_go_to_login_page(self, browser):
+        page = MainPage(browser, self.link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+        page.open()  # открываем страницу
+        page.should_be_login_link()  # проверяем что есть ссылка для перехода на страницу логина
+
+    def test_guest_should_see_login_link(self, browser):
+        page = MainPage(browser, self.link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+        page.open()  # открываем страницу
+        page.go_to_login_page()  # переходим на страницу логина
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
+
 
 def test_guest_can_go_to_login_page(browser):
     link = link_main_page
